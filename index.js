f3c4b25cc85820b7cf313f11524a30798b619dc1
anupriya-tech <84323931+anupriya-tech@users.noreply.github.com>
2021-05-25 23:35:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+//import React,{Component} from 'react';
+//import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);                                                           
